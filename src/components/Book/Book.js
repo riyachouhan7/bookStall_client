1579637bc90cfafb2ed5aad87273fe3e81e68abc
Kumari Riya <riyachouhan.ee@gmail.com>
2023-08-12 15:22:02 +0530

    lo commit

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 import { Button } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import "./Book.css";
-const BASE_URL = process.env.BASE_URL;
+
 
 const Book = (props) => {
     const history = useNavigate();
     const { _id, name, author, description, price, image } = props.book;
     const deleteHandler = async() => {
     await axios
-    .delete(`${BASE_URL}/books/${_id}`)
+    .delete(`http://localhost:5000/books/${_id}`)
     .then(res=>res.data)
     .then(() => history("/"))
     .then(()=> history("/books"));
@@ -21,7 +21,7 @@ const Book = (props) => {
         <h3>{name}</h3>
         <p>{description}</p>
         <h3>Rs. {price}</h3>
-        <Button LinkComponent={Link} to={`/books/${_id}`}sx={{ mt : 'auto' }}>
+        <Button LinkComponent={Link} to={`http://localhost:5000git /books/${_id}`}sx={{ mt : 'auto' }}>
             Update
         </Button>
         <Button onClick={ deleteHandler }sx={{ mt : 'auto' }}>
